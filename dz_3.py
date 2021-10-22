d61dc02ee1e5abbb93d5632153a5d625abf8ff5a
andrei <andrey.machnev@lynkage.tu>
2021-10-22 13:10:03 +0300

    22.10.2021

---
+++ dz_3.py
@@ -0,0 +1,45 @@
+# сашин код
+# def translate(word):
+#     names = {"zero": "ноль",
+#             "one": "один",
+#             "two":"два",
+#             "three":"три",
+#             "four":"четыре",
+#             "five":"пять",
+#             "six":"шесть",
+#             "seven":"семь",
+#             "eight":"восемь",
+#             "nine":"девять",
+#             "ten":"десять"}
+#     if word.istitle():
+#         another_word = word.lower()
+#         return str(names.get(another_word)).capitalize()
+#     else:
+#         return names.get(word)
+#
+#
+# hey = input('Введите числительное от 0 до 10 на английском\n')
+# print('Перевод:', translate(hey))
+#
+# димин код
+# translate_dict = {
+#         'zero': 'ноль',
+#         'one': 'один',
+#         'two': 'два',
+#         'three': 'три',
+#         'four': 'четыре',
+#         'five': 'пять',
+#         'six': 'шесть',
+#         'seven': 'семь',
+#         'eight': 'восемь',
+#         'nine': 'девять',
+#         'ten': 'десять'
+#     }
+#
+#     key = input("введите число от 0 до 10 для перевода с анлийского на русский язык: ")
+#     if key.istitle():
+#         print(translate_dict.get(key.lower()).title())
+#     else:
+#         print(translate_dict.get(key))
+
+
